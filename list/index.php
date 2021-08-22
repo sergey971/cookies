33f1e56504f8cf3f04082f55cb49192b5f3493d4
Sergey <murin.89@list.ru>
2021-08-22 23:20:24 +0300

    Cookie
    Создали новый код по созданию Cookie. Добавление и удалению.

--- list/index.php
+++ list/index.php
@@ -1,21 +1,64 @@
 <?php
-$cookie_name = "Пользователь";
-$cookie_value = "Иван Петров";
-setcookie($cookie_name, $cookie_value, time() + (86400 * 30), "/");
+
+error_reporting(E_ALL);
+
+if (!empty($_POST['add'])) {
+   setcookie('submit', $_POST['add'], time() + 3600, '/');
+   echo 'Кука создана';
+}
+
+if (!empty($_POST['delete'])) {
+   setcookie('submit', $_POST['add'], time() - 3600, '/');
+   echo 'Кука удаленна';
+}
+
+
 ?>
-<html>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <link rel="stylesheet" href="css/style.css">
+   <title>Task list</title>
+</head>
 
 <body>
+   <form action="" method="POST">
 
-   <?php
-   if (!isset($_COOKIE[$cookie_name])) {
-      echo "Cookie по имени'" . $cookie_name . "' не задан!";
-   } else {
-      echo "Cookie '" . $cookie_name . "' задан!<br>";
-      echo "Значение: " . $_COOKIE[$cookie_name];
-   }
-   ?>
+      <h1>Task list</h1>
+      <hr>
+      <div>
+         <p>
+            <input type="text" name="add">
+            <button type=submit name="submit" value=''>ADD TASK</button>
+         </p>
+      </div>
 
+      <div>
+         <p>
+
+            <input type="reset" name="remove_all" value=" REMOVE ALL" class="b2">
+            <input type="submit" name="ready_all" value=" READY ALL" class="b2">
+         </p>
+         <div class="block_item">
+            <div class="donat"></div>
+         </div>
+      </div>
+      <hr>
+
+      <p> <?php echo $_POST['add']; ?> </p>
+      <div>
+         <input type="submit" name="unready" value="UNREADY" class="b2">
+         <input type="submit" name="delete" value="DELETE" class="b2">
+         <div class="block_item">
+            <div class="donat1"></div>
+         </div>
+         <d1>
+      </div>
+      <hr>
+   </form>
 </body>
 
 </html>
